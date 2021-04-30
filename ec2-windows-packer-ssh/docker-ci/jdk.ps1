8abbcc23a32b1130c1fb6694b8a8dfe65d4acb82
dfoley84 <83467582+dfoley84@users.noreply.github.com>
2021-04-30 21:31:49 +0100

    Add files via upload

---
+++ ec2-windows-packer-ssh/docker-ci/jdk.ps1
@@ -0,0 +1,23 @@
+$ProgressPreference = 'SilentlyContinue'
+$ErrorActionPreference = 'Stop'
+
+Start-Transcript -path ("C:\{0}.log" -f $MyInvocation.MyCommand.Name) -append
+
+$jdkVersion = "zulu8.19.0.1-jdk8.0.112-win_x64"
+
+Invoke-Webrequest "http://cdn.azul.com/zulu/bin/$jdkVersion.zip" -OutFile jdk.zip -UseBasicParsing
+
+New-Item -Type Directory C:\jdk-temp > $null
+Expand-Archive jdk.zip -DestinationPath C:\jdk-temp
+Remove-Item -Force jdk.zip
+
+New-Item -Type Directory C:\jdk > $null
+mv C:\jdk-temp\$jdkVersion\* C:\jdk\.
+Remove-Item -Force -Recurse C:\jdk-temp
+
+$newPath = 'C:\jdk\bin;' + [Environment]::GetEnvironmentVariable("PATH", [EnvironmentVariableTarget]::Machine)
+[Environment]::SetEnvironmentVariable("PATH", $newPath, [EnvironmentVariableTarget]::Machine)
+
+C:\jdk\bin\java.exe -version
+
+Stop-Transcript
